d8e05056256b5a02c6f2af90b6d3230c23bc609b
Jalilov <javohirjalilov777@gmail.com>
2021-01-26 15:28:57 +0500

    add try and except

--- Randomuser_API/multiple.py
+++ Randomuser_API/multiple.py
@@ -5,16 +5,18 @@ import json
 payload = {'results':10}
 url = 'https://randomuser.me/api/'
 
-responce = requests.get(url,params=payload)
-data = responce.json()['results']
-
 data_list = []
-for x in data:
-        d = {}
-        d['Gender'] = x['gender']
-        d['First_name'] = x['name']['first']
-        d['Last_name'] = x['name']['last']
-        d['City'] = x['location']['city']
-        data_list.append(d)
+try:
+    responce = requests.get(url,params=payload)
+    data = responce.json()['results']
+    for x in data:
+            d = {}
+            d['Gender'] = x['gender']
+            d['First_name'] = x['name']['first']
+            d['Last_name'] = x['name']['last']
+            d['City'] = x['location']['city']
+            data_list.append(d)
+except:
+    print('Eror')
 
 pprint(data_list)
