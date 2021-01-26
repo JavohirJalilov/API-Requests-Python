f59e92f66b878bd664f286886942865a087e0318
Jalilov <javohirjalilov777@gmail.com>
2021-01-26 15:33:54 +0500

    write in json file

--- Randomuser_API/multiple.py
+++ Randomuser_API/multiple.py
@@ -2,7 +2,7 @@ import requests
 from pprint import pprint
 import json
 
-payload = {'results':10}
+payload = {'results':4}
 url = 'https://randomuser.me/api/'
 
 data_list = []
@@ -10,13 +10,14 @@ try:
     responce = requests.get(url,params=payload)
     data = responce.json()['results']
     for x in data:
-            d = {}
-            d['Gender'] = x['gender']
-            d['First_name'] = x['name']['first']
-            d['Last_name'] = x['name']['last']
-            d['City'] = x['location']['city']
-            data_list.append(d)
+        d = {}
+        d['Gender'] = x['gender']
+        d['First_name'] = x['name']['first']
+        d['Last_name'] = x['name']['last']
+        d['City'] = x['location']['city']
+        data_list.append(d)
+    f = open('answer.json','a')
+    data_json = json.dumps({"RequestingMultipleUsers":data_list})
+    f.write(data_json)
 except:
     print('Eror')
-
-pprint(data_list)
