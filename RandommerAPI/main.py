723f9337c541e401ed01037476c0b0f891723f6a
Jalilov <javohirjalilov777@gmail.com>
2021-01-27 18:57:10 +0500

    Carta type

--- RandommerAPI/main.py
+++ RandommerAPI/main.py
@@ -3,7 +3,9 @@ import requests
 key = '66c318d009dd44edbf4cc378f81c8dbc'
 headers = {'X-Api-Key':key}
 url = 'https://randommer.io/api/Card'
+payload = {
+    'type':'Visa'
+}
+responce = requests.get(url,params=payload,headers=headers)
 
-responce = requests.get(url,headers=headers)
-
-print(responce.status_code)
+print(responce.json())
