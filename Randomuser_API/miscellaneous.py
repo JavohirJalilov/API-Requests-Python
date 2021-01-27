c96be644114f6e650c1d8ca1ef176e06f181d9e8
Javokhir <71634218+JavohirJalilov@users.noreply.github.com>
2021-01-27 17:42:29 +0500

    Delete miscellaneous.py

--- Randomuser_API/miscellaneous.py
+++
@@ -1,9 +0,0 @@
-import requests
-from pprint import pprint
-import json
-
-payload = {'results':4,'inc':'gender,name,nat','nat':'US'}
-url = 'https://randomuser.me/api/'
-responce = requests.get(url=url,params=payload)
-
-pprint(responce.json())
