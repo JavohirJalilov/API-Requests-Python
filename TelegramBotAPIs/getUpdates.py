6d9edc4f179d6e9d6db1bb82443403cdd6a2d9da
Jalilov <javohirjalilov777@gmail.com>
2021-01-31 17:22:00 +0500

    Find full_name

--- TelegramBotAPIs/getUpdates.py
+++ TelegramBotAPIs/getUpdates.py
@@ -7,4 +7,15 @@ TOKEN = os.environ['TOKEN']
 responce = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
 
 data = responce.json()
-pprint(len(data['result']))
+updates = data['result']
+
+for update in updates:
+    message = update['message']
+    user = message['from']
+    last_name = user.get('last_name',False)
+    first_name = user['first_name']
+    if last_name:
+        full_name = first_name + " " + last_name
+    else:
+        full_name = first_name
+    print(full_name)
