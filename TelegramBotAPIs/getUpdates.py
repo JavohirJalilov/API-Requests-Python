847d833260a70a773de4eda30fcf8a88d40901ab
Jalilov <javohirjalilov777@gmail.com>
2021-01-31 16:52:13 +0500

    result data

---
+++ TelegramBotAPIs/getUpdates.py
@@ -0,0 +1,10 @@
+import requests
+import os
+from pprint import pprint
+
+TOKEN = os.environ['TOKEN']
+
+responce = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
+
+data = responce.json()
+pprint(data)
