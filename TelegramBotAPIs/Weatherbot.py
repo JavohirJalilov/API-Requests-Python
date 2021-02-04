57c90943a75134e193f3eb09513e9f1a64f233c1
Jalilov <javohirjalilov777@gmail.com>
2021-02-04 15:33:58 +0500

    sendMessage weather

--- TelegramBotAPIs/Weatherbot.py
+++ TelegramBotAPIs/Weatherbot.py
@@ -14,5 +14,30 @@ def getUpdates():
 
     return chat_id,text
 
-print(getUpdates())
-    
+def getWeather(text):
+    payload = {
+        'q':text,
+        'appid':KEY
+    }
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    responce = requests.get(url,params=payload).json()
+    name = responce['name']
+    description = responce['weather'][0]['description']
+    icon = responce['weather'][0]['icon']
+    temp = round(responce['main']['temp'] - 273)
+
+    text = f"From: {name}\nDescription: {description}\nTemp: {temp}\nIcon: {icon}"
+    return text
+
+
+
+def sendMessage(chat_id,text):
+    payload = {
+        'chat_id':chat_id,
+        'text':text
+    }
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    requests.get(url,payload)
+
+chat_id,text = getUpdates()
+sendMessage(chat_id,getWeather(text))
