aa5c932a296102dbd28afcceed5b9ae93c8610c6
Jalilov <javohirjalilov777@gmail.com>
2021-02-07 17:05:10 +0500

    send through thumb URL

---
+++ TelegramBotAPIs/sendPhoto.py
@@ -0,0 +1,23 @@
+import requests
+import os
+from pprint import pprint
+
+TOKEN = os.environ['TOKEN']
+def get_updates():
+    responce = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
+    data = responce.json()['result'][-1]
+    chat_id = data['message']['chat']['id']
+
+    return chat_id
+
+def sendPhoto(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
+    payload = {
+        'chat_id':chat_id,
+        'photo':'https://random.dog/a1eba572-e557-474b-a023-e48ead3c2786.jpeg'
+    }
+    r = requests.get(url,payload)
+    pprint(r.json())
+# 1046157991
+
+sendPhoto(1046157991)
