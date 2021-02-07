cef05216b21739f17f0925615cc79651dd05f611
Jalilov <javohirjalilov777@gmail.com>
2021-02-07 16:40:13 +0500

    count Like and Dislike

--- TelegramBotAPIs/likebot.py
+++ TelegramBotAPIs/likebot.py
@@ -18,15 +18,25 @@ def LikeDisLike():
     dislike = '\U0001F44E'
     return like,dislike
 
+list1 = []
+list2 = []
 def writeLike(chat_id,text,first_name):
-    print(text)
+    like,dislike = LikeDisLike()
+    if text == like:
+        list1.append(like)
+    else:
+        list2.append(dislike)
 
 last_update_id = -1
 
+def count():
+    print(f'Like: {list1}\nDislike: {list2}')
 while True:
     chat_id,update_id,chat_id,text,first_name = getUpdates()
     like,dislike = LikeDisLike()
     if last_update_id != update_id:
         if text == like or text == dislike:
             writeLike(chat_id,text,first_name)
+        if text == '/count':
+            count()
         last_update_id = update_id
