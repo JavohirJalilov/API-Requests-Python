681bbadc690a92879a1473ac92a769cc83491f8c
Jalilov <javohirjalilov777@gmail.com>
2021-02-08 13:30:58 +0500

    resize keyboard

--- TelegramBotAPIs/sendKeyboard.py
+++ TelegramBotAPIs/sendKeyboard.py
@@ -12,18 +12,25 @@ def getUpdates():
     return chat_id,text
 
 def sendMessage(chat_id,text):
-    button1 = {'text':'button1'}
-    button2 = {'text':'button2'}
-    keyboard = [[button1,button2]]
-    reply_markup = {'keyboard':keyboard}
+    button1 = {
+        'text':'button1'
+        }
+    button2 = {
+        'text':'button2'
+        }
+    keyboard = [
+        [button1,button2],
+        [button1]
+        ]
+    reply_markup = {'keyboard':keyboard,'resize_keyboard':True}
     payload = {
         'chat_id':chat_id,
         'text': text,
         'reply_markup':reply_markup
     }
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    r = requests.get(url,json=payload)
-    pprint(r.json())
+    responce = requests.get(url,json=payload)
+    pprint(responce.json())
 # 1046157991
 chat_id,text = getUpdates()
 sendMessage(chat_id,text)
