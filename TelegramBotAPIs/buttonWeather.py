71c2e778dec905d18a7b8ea9c46e75ac56f0c47a
Jalilov <javohirjalilov777@gmail.com>
2021-02-10 16:03:07 +0500

    Location bot finally and

--- TelegramBotAPIs/buttonWeather.py
+++ TelegramBotAPIs/buttonWeather.py
@@ -14,14 +14,32 @@ def getUpdates()->tuple:
 
     return chat_id,text,update_id
 
-def getLocation():
+def getLocation()->tuple:
     responce = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
     data = responce.json()['result'][-1]
     location = data['message'].get('location')
     lat = location.get('latitude')
     lan = location.get('longitude')
-    print(lat,lan)
     return lat,lan
+
+def getWeather(lat:float,lon:float)->str:
+    payload = {
+        'lat':lat,
+        'lon':lon,
+        'appid':KEY
+    }
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    responce = requests.get(url,params=payload)
+    if responce.status_code == 200:
+        responce = responce.json()
+        name = responce['name']
+        description = responce['weather'][0]['description']
+        icon = responce['weather'][0]['icon']
+        temp = round(responce['main']['temp'] - 273)
+        text = f"From: {name}\nDescription: {description}\nTemp: {temp}\nIcon: {icon}"
+    else:
+        text = 'Bunday shahar mavjud emas.\nIltimos qaytadan urinib ko\'ring'
+    return text
 
 def sendMessage(chat_id:int,text:str):
     button = {'text':'Location ','request_location':True}
@@ -35,7 +53,19 @@ def sendMessage(chat_id:int,text:str):
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
     requests.get(url,json=payload)
 
-chat_id,text,update_id = getUpdates()
-location = getLocation()
-
-
+last_update_id = -1
+while True:
+    chat_id,text,update_id = getUpdates()
+    if last_update_id != update_id:
+        if text == '/start':
+            text = 'Location Kiriting !'
+            sendMessage(chat_id,text)
+        elif text == None:
+            lat,lon = getLocation()
+            text = getWeather(lat,lon)
+            sendMessage(chat_id,text)
+        else:
+            text = 'Iltimos, Location Kiriting.'
+            sendMessage(chat_id,text)
+            
+        last_update_id = update_id
